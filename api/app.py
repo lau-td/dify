0d5eeb7897ec89afc527129d71f36ad4fabe3c7b
lau.td <lau.td.developer@gmail.com>
2024-09-04 16:42:10 +0700

    feat: enable cors

--- api/app.py
+++ api/app.py
@@ -246,7 +246,9 @@ def register_blueprints(app):
 
     CORS(
         codelight_bp,
-        allow_headers=["Content-Type", "Authorization", "X-App-Id"],
+        resources={r"/*": {"origins": "*"}},
+        supports_credentials=True,
+        allow_headers=["Content-Type", "Authorization"],
         methods=["GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"],
     )
     app.register_blueprint(codelight_bp)
