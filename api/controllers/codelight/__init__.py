0b1e00710e2889b576ff1665513f972f6e891abd
nguyenphuvinhtoan <nguyenphuvinhtoan@gmail.com>
2024-09-19 15:59:31 +0700

    feat: pagination chat message history

--- api/controllers/codelight/__init__.py
+++ api/controllers/codelight/__init__.py
@@ -5,6 +5,6 @@ bp = Blueprint("codelight_api", __name__, url_prefix="/v1/codelight")
 api = ExternalApi(bp)
 
 
-from .console_api import dataset
+from .console_api import dataset, message
 from .internal_api import account, app, auth, model_config, model_providers, tenant
 from .web import passport, conversation
