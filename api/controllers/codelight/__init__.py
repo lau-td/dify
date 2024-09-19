261a65fca556af3c70f96210bcecc0044389f74d
TrungPham <trungphamdac201@gmail.com>
2024-09-19 11:17:25 +0700

    feat: login-without-password

--- api/controllers/codelight/__init__.py
+++ api/controllers/codelight/__init__.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
-
 from libs.external_api import ExternalApi
 
 bp = Blueprint("codelight_api", __name__, url_prefix="/v1/codelight")
 api = ExternalApi(bp)
 
 
-from .internal_api import account, tenant, model_providers, app, model_config, auth
+from .console_api import dataset
+from .internal_api import (account, app, auth, model_config, model_providers,
+                           tenant)
