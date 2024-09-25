9fc3c1a7e6604c59a1fb73030cc28069ef2ccadc
nguyenphuvinhtoan <nguyenphuvinhtoan@gmail.com>
2024-09-25 17:07:28 +0700

    adding new get conversations api in v1/codelight

--- api/controllers/codelight/__init__.py
+++ api/controllers/codelight/__init__.py
@@ -5,6 +5,6 @@ bp = Blueprint("codelight_api", __name__, url_prefix="/v1/codelight")
 api = ExternalApi(bp)
 
 
-from .console_api import dataset, message
+from .console_api import dataset, message, conversation
 from .internal_api import account, app, auth, model_config, model_providers, tenant
 from .web import passport, conversation
