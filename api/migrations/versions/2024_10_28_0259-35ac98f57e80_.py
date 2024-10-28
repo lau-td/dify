84f3a1ab1d7cf7349b7159326023de7fb478b045
lau.td <lau.td.developer@gmail.com>
2024-10-28 10:09:08 +0700

    update migration files

---
+++ api/migrations/versions/2024_10_28_0259-35ac98f57e80_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: 35ac98f57e80
+Revises: b8a56b978fc6, bbadea11becb
+Create Date: 2024-10-28 02:59:42.584826
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '35ac98f57e80'
+down_revision = ('b8a56b978fc6', 'bbadea11becb')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
